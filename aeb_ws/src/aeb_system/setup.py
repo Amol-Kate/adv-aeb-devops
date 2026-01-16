f3f2bb1f0f07aaaa5c13df65e5df97b15d9b4823
Amol-Kate <148143149+Amol-Kate@users.noreply.github.com>
2026-01-16 23:40:19 +0530

    Create setup.py

---
+++ aeb_ws/src/aeb_system/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+package_name = 'aeb_system'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=[package_name],
+    data_files=[
+        # Required by ament_python
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='AEB Dev',
+    maintainer_email='dev@example.com',
+    description='Autonomous Emergency Braking (AEB) ROS 2 Python package',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'camera_node = aeb_system.camera_node:main',
+            'pedestrian_detector = aeb_system.pedestrian_detector:main',
+            'aeb_decision_node = aeb_system.aeb_decision_node:main',
+            'vehicle_cont_
